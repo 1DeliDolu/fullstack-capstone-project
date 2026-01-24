2ceeb474af8b6c3389c099d6dd01a1cedad9c3c7
Mustafa Oezdemir <mustafa.ozdemir1408@gmail.com>
2026-01-24 14:22:00 -0500

    a

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
+import MainPage from './components/SearchPage/SearchPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
+          <Route path="/search" element={<SearchPage />} />
           <Route path="/login" element={<LoginPage/>} />
           <Route path="/register" element={<RegisterPage />} />
         </Routes>
